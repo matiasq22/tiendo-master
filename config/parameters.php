53522dbebfa09740ed4eb344840ec6c116f2ae88
Matias Quevedo <mquevedo@unnaki.com.py>
2020-02-12 11:59:37 -0300

    fix

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-$url =  getenv('BASE_URL') ? getenv('BASE_URL') : 'localhost';
+$url =  getenv('BASE_URL') ? getenv('BASE_URL') : 'http:/localhost/';
 define("base_url", $url);
 define("controller_default","ProductsController");
 define("action_default","index");
